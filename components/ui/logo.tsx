7b706b9e3d0df2f3634ee17c9cc1e91df0e9ab0a
dragosnedelcu <dragos199993@gmail.com>
2023-12-31 15:04:31 +0200

    Added badge.

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import Link from 'next/link'
 import { routes } from '@/constants/routes'
+import { Badge } from '@/components/ui/badge'
 
 export const Logo = () => {
   return (
-    <Link
-      href={routes.HOME}
-      className="flex scroll-m-20 items-center gap-2 text-3xl font-extrabold tracking-tight lg:text-4xl"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="30"
-        height="30"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="lucide lucide-home"
+    <div>
+      <Link
+        href={routes.HOME}
+        className="flex scroll-m-20 items-center gap-2 text-3xl font-extrabold tracking-tight lg:text-4xl"
       >
-        <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-        <polyline points="9 22 9 12 15 12 15 22" />
-      </svg>
-      {process.env.NEXT_PUBLIC_APP_NAME}
-    </Link>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-home"
+        >
+          <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+          <polyline points="9 22 9 12 15 12 15 22" />
+        </svg>
+        {process.env.NEXT_PUBLIC_APP_NAME}
+      </Link>
+      <Badge variant="destructive">Staging</Badge>
+    </div>
   )
 }
