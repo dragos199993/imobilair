c7b2b860cd62999b5ff6f3a24f432e1ea53ddd47
dragosnedelcu <dragos199993@gmail.com>
2023-12-31 17:07:13 +0200

    Added badge.

--- app/[locale]/(landing)/_components/HeroVideo.tsx
+++ app/[locale]/(landing)/_components/HeroVideo.tsx
@@ -6,7 +6,10 @@ export const HeroVideo = () => {
       className="mx-auto mb-12 h-full w-full max-w-[750px] rounded-none shadow-2xl sm:rounded-2xl"
       controls
     >
-      <source src="/hero-video.mp4" type="video/mp4" />
+      <source
+        src="https://assets.imobilair.com/hero-video.mp4"
+        type="video/mp4"
+      />
       Your browser does not support the video tag.
     </video>
   )
