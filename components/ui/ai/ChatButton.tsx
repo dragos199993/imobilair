89ef154c8b28bdbc8785dd70c6df0d583bff61b8
dragosnedelcu <dragos199993@gmail.com>
2024-01-04 23:19:32 +0200

    Chatbox mobile.

--- components/ui/ai/ChatButton.tsx
+++ components/ui/ai/ChatButton.tsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react'
 import { Chatbox } from '@/components/ui/ai/Chatbox'
 import { Button } from '@/components/ui/button'
 import { Bot } from 'lucide-react'
+import { cn } from '@/lib/utils'
 
-export const ChatButton = () => {
+type Props = {
+  hideLabel?: boolean
+}
+
+export const ChatButton = ({ hideLabel = false }: Props) => {
   const [open, setOpen] = useState(false)
+
   return (
     <>
       <Button onClick={() => setOpen(true)}>
-        <Bot size={20} className="mr-2" />
-        AI Chat
+        <Bot size={20} className={cn(!hideLabel && 'mr-2')} />
+        {!hideLabel && 'AI Chat'}
       </Button>
-      <Chatbox open={open} onClose={() => setOpen(false)} />
+      <Chatbox open={open} setOpen={setOpen} />
     </>
   )
 }
--- components/ui/ai/Chatbox.tsx
+++ components/ui/ai/Chatbox.tsx
@@ -8,14 +8,67 @@ import { Message } from 'ai'
 import { useOnClickOutside } from 'usehooks-ts'
 import { useTranslations } from 'next-intl'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
+import { useMediaQuery } from '@/hooks/use-media-query'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
 
 type Props = {
   open: boolean
-  onClose: () => void
+  setOpen: (value: boolean) => void
 }
 
-export const Chatbox = ({ onClose, open }: Props) => {
+export const Chatbox = ({ setOpen, open }: Props) => {
   const t = useTranslations('Dashboard')
+  const isDesktop = useMediaQuery('(min-width: 768px)')
+  const ref = useRef(null)
+  useOnClickOutside(ref, () => setOpen(false))
+
+  if (isDesktop) {
+    return (
+      <div
+        ref={ref}
+        className={cn(
+          'relative bottom-0 right-0 z-40 w-full max-w-[500px] rounded-lg bg-secondary shadow-xl xl:right-36',
+          open ? 'fixed' : 'hidden'
+        )}
+      >
+        <Button
+          variant="ghost"
+          title="Close"
+          className="absolute right-0 top-2"
+          onClick={() => setOpen(false)}
+        >
+          <X size={30} />
+        </Button>
+
+        <ChatBase open={open} setOpen={setOpen} isDesktop={isDesktop} />
+      </div>
+    )
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>{t('chat_box_title')}</DrawerTitle>
+          </DrawerHeader>
+          <ChatBase open={open} setOpen={setOpen} isDesktop={isDesktop} />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+const ChatBase = ({ open, isDesktop }: Props & { isDesktop: boolean }) => {
   const {
     messages,
     input,
@@ -29,8 +82,6 @@ export const Chatbox = ({ onClose, open }: Props) => {
   })
   const scrollRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLInputElement>(null)
-  const ref = useRef(null)
-  useOnClickOutside(ref, onClose)
 
   useEffect(() => {
     if (scrollRef.current) {
@@ -46,69 +97,56 @@ export const Chatbox = ({ onClose, open }: Props) => {
 
   const isLastMessageUser = messages[messages.length - 1]?.role === 'user'
 
+  const t = useTranslations('Dashboard')
+
   return (
-    <div
-      ref={ref}
-      className={cn(
-        'relative bottom-0 right-0 z-40 w-full max-w-[500px] rounded-lg bg-secondary shadow-xl xl:right-36',
-        open ? 'fixed' : 'hidden'
-      )}
-    >
-      <Button
-        variant="ghost"
-        title="Close"
-        className="absolute right-0 top-2"
-        onClick={onClose}
-      >
-        <X size={30} />
-      </Button>
-
-      <div className="flex h-[600px] flex-col border bg-secondary shadow-xl">
+    <div className="flex h-[600px] flex-col border bg-secondary shadow-xl">
+      {isDesktop && (
         <p className="pl-4 pt-4 text-lg font-semibold">{t('chat_box_title')}</p>
-        <div className="mt-8 h-full overflow-y-auto p-3" ref={scrollRef}>
-          {!!messages.length &&
-            !error &&
-            messages.map((message) => (
-              <ChatMessage key={message.id} message={message} />
-            ))}
+      )}
+      <div className="mt-8 h-full overflow-y-auto p-3" ref={scrollRef}>
+        {!!messages.length &&
+          !error &&
+          messages.map((message) => (
+            <ChatMessage key={message.id} message={message} />
+          ))}
 
-          {!messages.length && !error && (
-            <div className="flex h-full items-center justify-center gap-2">
-              <Bot size={20} /> Start a conversation here
-            </div>
-          )}
-          {isLoading && isLastMessageUser && (
-            <ChatMessage
-              message={{
-                role: 'assistant',
-                content: 'Thinking...',
-              }}
-            />
-          )}
-
-          {error && (
-            <ChatMessage
-              message={{
-                role: 'system',
-                content: 'Something went wrong',
-              }}
-            />
-          )}
-        </div>
-        <form onSubmit={handleSubmit} className="m-3 flex gap-1">
-          <Button variant="ghost" type="button" onClick={() => setMessages([])}>
-            <Trash />
-          </Button>
-
-          <Input
-            ref={inputRef}
-            value={input}
-            onChange={handleInputChange}
-            placeholder="Say something..."
+        {!messages.length && !error && (
+          <div className="flex h-full items-center justify-center gap-2">
+            <Bot size={20} /> Start a conversation here
+          </div>
+        )}
+        {isLoading && isLastMessageUser && (
+          <ChatMessage
+            message={{
+              role: 'assistant',
+              content: 'Thinking...',
+            }}
           />
-          <Button type="submit">Send</Button>
-        </form>
+        )}
+
+        {error && (
+          <ChatMessage
+            message={{
+              role: 'system',
+              content: 'Something went wrong',
+            }}
+          />
+        )}
       </div>
+      <form onSubmit={handleSubmit} className="m-3 flex gap-2">
+        <Button variant="ghost" type="button" onClick={() => setMessages([])}>
+          <Trash />
+        </Button>
+
+        <Input
+          ref={inputRef}
+          value={input}
+          onChange={handleInputChange}
+          placeholder="Say something..."
+        />
+        <Button type="submit">Send</Button>
+      </form>
     </div>
   )
 }
